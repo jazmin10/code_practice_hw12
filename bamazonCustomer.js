740edcba16d21d944b25deb9ed96cac58cd9872f
jazmin10 <jazminestrada10@gmail.com>
2018-07-09 17:05:04 -0500

    Added logic to find product's information once user answers prompt questions

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -21,6 +21,7 @@
 	// Returns an array of the list of items that are being sold
 	function setChoices(list) {
 
+		// Will store the array of the choices that will be used in the prompt questions
 		var choicesArr = [];
 
 		// loops through the list and ...
@@ -37,6 +38,43 @@
 		return choicesArr;
 	}
 
+	// Find the information of the product chosen by finding its object in the itemsArr
+	function findProduct(str) {
+
+		// Set the start index and end index of where we will be slicing our string
+		var start = str.indexOf(":") + 2;
+		var end = str.indexOf("|") - 1;
+
+		// Storing the name of the product chosen by slicing the string
+		// In other words, we want "apples" NOT "product: apples | price: 1.49"
+		var productName = str.slice(start, end);
+
+		// Will store the object of the product chosen
+		var product = {};
+
+		// Loop through our items array and ...
+		itemsArr.forEach(function(item) {
+
+			// If the product name of the object matches the productName, then set
+			// product to the current object
+			if (item.product_name === productName) {
+				product = item;
+			}
+		});
+
+		// Return the product's object
+		return product;
+
+	}
+
+	// Submit the purchase by...
+	function makePurchase(answers) {
+		
+		// Finding the information of the product chosen by the user
+		var productChosen = findProduct(answers.item);
+
+		connection.end();
+	}
 
 // ================== MAIN PROCESSES ==================
 
@@ -47,6 +85,10 @@
 		// Grab items stored in the database and ...
 		connection.query("SELECT * FROM products", function(err, readResults) {
 			if (err) throw err;
+
+			// Set itemsArr equal to readResults
+			itemsArr = readResults;
+
 
 			// Set the questions that will be asked to the user
 			var mainPrompt = [{
@@ -61,11 +103,8 @@
 				message: "How many?"
 			}];
 
-			// Ask the user the product they want to purchase and quantity
-			inquirer.prompt(mainPrompt).then(function(answers) {
-				console.log(answers);
-				connection.end();
-			});
+			// Ask the user the product they want to purchase and quantity, then make purchase
+			inquirer.prompt(mainPrompt).then(makePurchase);
 
 		});
 		
