bb5cd715d4da37dc96329065ab0ae079a75116c3
jazmin10 <jazminestrada10@gmail.com>
2018-07-09 20:35:24 -0500

    Added js logic that prompts user to pick a command for manager view

--- bamazonManager.js
+++ bamazonManager.js
@@ -1,7 +1,9 @@
 // ================== GLOBAL VARIABLES ==================
+	// Import packages
 	var inquirer = require("inquirer");
 	var mysql = require("mysql");
 
+	// Setting up connection
 	var connection = mysql.createConnection({
 		host: "localhost",
 		port: 3306,
@@ -10,13 +12,47 @@
 		database: "bamazon2_db"
 	});
 
+	// Prompt questions
+	var mainPrompt = [{
+		name: "command",
+		type: "list",
+		message: "Pick an option: ",
+		choices: [
+			"View Products for Sale", 
+			"View Low Inventory", 
+			"Add to Inventory", 
+			"Add New Product"
+		]
+	}];
+
 // ================== FUNCTIONS ==================
 
 // ================== MAIN PROCESSES ==================
 
-connection.connect(function(err) {
-	if (err) throw err;
+	// Connect to the database
+	connection.connect(function(err) {
+		if (err) throw err;
 
-	console.log("connected as id " + connection.threadId);
-	connection.end();
-});
+		// Ask user the command they would like to execute
+		inquirer.prompt(mainPrompt).then(function(answer) {
+			
+			// Execute the command chosen by the user
+			switch (answer.command) {
+				case "View Products for Sale":
+					console.log("view all products");
+					break;
+				case "View Low Inventory":
+					console.log("view products with less than 5 items");
+					break;
+				case "Add to Inventory":
+					console.log("update inventory");
+					break;
+				case "Add New Product":
+					console.log("add a product");
+					break;
+			}
+
+			connection.end();
+
+		});
+	});
